8005e24f81d6ae8c696ce040658941f531dc43e7
siddhant-sri <sidz.dec26@gmail.com>
2024-06-20 11:54:27 +0530

    Memoization of data in apps to optimize performance

--- src/hooks/useNowPlayingMovies.jsx
+++ src/hooks/useNowPlayingMovies.jsx
@@ -1,4 +1,4 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { API_OPTIONS } from "../utils/constants";
 import { addNowPlayingMovies } from "../utils/moviesSlice";
 import { useEffect } from "react";
@@ -6,6 +6,10 @@ import { useEffect } from "react";
 const useNowPlayingMovies = () => {
   // Fetch data form TMDB API and update the store
   const dispatch = useDispatch();
+
+  const nowPlayingMovies = useSelector(
+    (store) => store.movies.nowPlayingMovies
+  );
 
   const getNowPlayingMovies = async () => {
     const data = await fetch(
@@ -20,7 +24,7 @@ const useNowPlayingMovies = () => {
   };
 
   useEffect(() => {
-    getNowPlayingMovies();
+    !nowPlayingMovies && getNowPlayingMovies(); // OR if(!nowPlayingMovies)  getNowPlayingMovies();
   }, []);
 };
 
