312fc634808faae88c966819e7359f784fd2b46f
siddhant-sri <sidz.dec26@gmail.com>
2024-06-20 09:37:26 +0530

    Secured Firebase configs

--- src/utils/firebase.jsx
+++ src/utils/firebase.jsx
@@ -8,7 +8,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCMMl6SxDnWfVZB_ioYXmti4iemgQGqyBc",
+  apiKey: import.meta.env.VITE_FIREBASE_KEY,
   authDomain: "netflixgpt-026.firebaseapp.com",
   projectId: "netflixgpt-026",
   storageBucket: "netflixgpt-026.appspot.com",
